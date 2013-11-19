660de657af1a1158da1232989863fb55a585a21e
dcristian <dan.cristian@gmail.com>
2013-11-19 22:51:56 +0200

    leap

--- MZP-NET/MultiZonePlayer/Scripts/slideshow.bat
+++ MZP-NET/MultiZonePlayer/Scripts/slideshow.bat
@@ -1,1 +1,8 @@
-"C:\Program Files\IrfanView\i_view32.exe" /one /fs /slideshow=c:\slideshow.txt
+rem iexplore -k http://192.168.0.10:888/m/slide.html
+
+ping localhost -n 25
+rem start /MIN chrome.exe --homepage http://www.google.com
+rem ping localhost -n 15
+rem taskkill /IM chrome.exe
+rem start chrome.exe --kiosk --homepage http://192.168.0.10/m/slide.html 
+start iexplore -k http://192.168.0.10/m/slide.html
--- MZP-NET/WebApp/WebApp/m/leap.js
+++ MZP-NET/WebApp/WebApp/m/leap.js
@@ -4,6 +4,7 @@ var pauseOnGesture = false;
 var presenceSince = new Date();
 var secondsSince;
 var isPresenceOn = true;
+var s = document.createElement('div');
 
 // Support both the WebSocket and MozWebSocket objects
 if ((typeof (WebSocket) == 'undefined') &&
@@ -14,7 +15,7 @@ if ((typeof (WebSocket) == 'undefined') &&
 // Create the socket with event handlers
 function init() {
 	// Create and open the socket
-	ws = new WebSocket("ws://localhost:6437/");
+	ws = new WebSocket("ws://192.168.0.10:6437/"); 
 
 	// On successful connection
 	ws.onopen = function (event) {
@@ -24,47 +25,68 @@ function init() {
 		document.getElementById("connection").innerHTML = "OK";
 	};
 
+	followCursor.init();
+	document.body.onmousemove = followCursor.run;
+
 	// On message received
 	ws.onmessage = function (event) {
+		secondsSince = Math.round((new Date() - presenceSince) / 1000);
+		document.getElementById("counter").innerHTML = secondsSince;
 		if (!paused) {
 			var obj = JSON.parse(event.data);
 			var str = JSON.stringify(obj, undefined, 2);
 			//document.getElementById("output").innerHTML = '<pre>' + str + '</pre>';
-			if (pauseOnGesture && obj.gestures.length > 0) {
-				togglePause();
-				//document.getElementById("gest").innerHTML += obj.gestures[0].type +';';
-			}
+			if (obj != null) {
+				if (pauseOnGesture && obj.gestures.length > 0) {
+					togglePause();
+					//document.getElementById("gest").innerHTML += obj.gestures[0].type +';';
+				}
 
-			if (obj.gestures != null && obj.gestures.length > 0) {
-				if (obj.gestures[0].state == "stop" && obj.gestures[0].duration > 500999) {
-					//document.getElementById("gest").innerHTML += '.';
-					if (obj.gestures[0].type == "circle") {
-						document.getElementById("leap").innerHTML = "circle";
-						next();
-						document.getElementById("leap").innerHTML = "circle:" + obj.gestures[0].duration;
+				if (obj.gestures != null && obj.gestures.length > 0 && obj.gestures[0].duration > 500999) {
+					if (obj.gestures[0].state == "stop") {
+						//document.getElementById("gest").innerHTML += '.';
+						if (obj.gestures[0].type == "circle") {
+							//document.getElementById("leap").innerHTML = "circle";
+							//next();
+							//document.getElementById("leap").innerHTML = "circle:" + obj.gestures[0].duration;
+						}
+					}
+					document.getElementById("leap1").innerHTML = "Cmd:" + obj.gestures[0].type + "-" + obj.gestures[0].state + "-" + obj.gestures[0].duration;
+				}
+
+				if (obj.gestures != null && obj.gestures.length > 0) {
+					//document.getElementById("leap2").innerHTML = "Real:" + obj.gestures[0].type + "-" + obj.gestures[0].state + "-" + obj.gestures[0].duration;
+				}
+
+				if (obj.pointables != null && obj.hands != null) {
+					document.getElementById("leap3").innerHTML = "Hands: " + obj.hands.length + " fingers: " + obj.pointables.length;
+					if (obj.hands.length == 1) {
+						var x = obj.hands[0].palmPosition[0];
+						var y = obj.hands[0].palmPosition[2];
+						//document.getElementById('msg').innerHTML = x + ', ' + y;
+						s.style.left = 200 + (x*1.2) + 'px';
+						s.style.top = 200 + (y*1.2) + 'px';
 					}
 				}
-				else
-					document.getElementById("leap").innerHTML = obj.gestures[0].type + "-" + obj.gestures[0].state;
-			}
-				
-			if (obj.hands != null && obj.hands.length == 1 && obj.pointables != null && obj.pointables.length == 5) {
-				//document.getElementById("hand").innerHTML = obj.hands.length + ';';
-				presenceOn();
-				isPresenceOn = true;
-				presenceSince = new Date();
-			}
-			else {
-				if (isPresenceOn) {
-					secondsSince = Math.round((new Date() - presenceSince) / 1000);
-					if (secondsSince > 10) {
-						presenceOff();
-						isPresenceOn = false;
+
+				if (obj.hands != null && obj.hands.length == 1 && obj.pointables != null && obj.pointables.length == 5) {
+					//document.getElementById("hand").innerHTML = obj.hands.length + ';';
+					presenceOn();
+					isPresenceOn = true;
+					presenceSince = new Date();
+				}
+				else {
+					if (isPresenceOn) {
+						secondsSince = Math.round((new Date() - presenceSince) / 1000);
+						if (secondsSince > 30) {
+							presenceOff();
+							isPresenceOn = false;
+						}
 					}
 				}
-			}
-			if (obj.pointables != null) {
-				//document.getElementById("point").innerHTML = obj.pointables.length + ';';
+				if (obj.pointables != null) {
+					//document.getElementById("point").innerHTML = obj.pointables.length + ';';
+				}
 			}
 		}
 	};
@@ -99,3 +121,31 @@ function pauseForGestures() {
 		pauseOnGesture = false;
 	}
 }
+
+function getMouseCoords(e) {
+	var e = e || window.event;
+	//document.getElementById('msg').innerHTML = e.clientX + ', ' +
+    //       e.clientY + '<br>' + e.screenX + ', ' + e.screenY;
+}
+
+
+var followCursor = (function () {
+	//var s = document.createElement('div');
+	s.style.position = 'absolute';
+	s.style.margin = '0';
+	s.style.padding = '5px';
+	s.style.border = '2px solid red';
+	
+	return {
+		init: function () {
+			document.body.appendChild(s);
+		},
+
+		run: function (e) {
+			var e = e || window.event;
+			//s.style.left = (e.clientX - 5) + 'px';
+			//s.style.top = (e.clientY - 5) + 'px';
+			getMouseCoords(e);
+		}
+	};
+} ());
